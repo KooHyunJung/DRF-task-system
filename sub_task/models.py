a9692c3165285dd3793b144834728d3a008ee5c5
KooHyunJung <ghj3160@gmail.com>
2023-09-03 14:21:01 +0900

    add models for user task sub_task

--- sub_task/models.py
+++ sub_task/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class SudTask(models.Model):
+    id = models.IntegerField(primary_key=True)
+    team = models.CharField(max_length=30)
+    is_complete = models.BooleanField(default=False)
+    complete_date = models.DateField(null=True, blank=True)
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
--- task/models.py
+++ task/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user_id = models.IntegerField(max_length=30)
+    team = models.CharField(max_length=30)
+    title = models.CharField(max_length=30)
+    content = models.IntegerField(max_length=500)
+    is_complete = models.BooleanField(default=False)
+    complete_date = models.DateField(null=True, blank=True)
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
