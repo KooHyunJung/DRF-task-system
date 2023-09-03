1b343ef979e99658f3bbda10eb833e78cf7e7241
KooHyunJung <ghj3160@gmail.com>
2023-09-03 22:55:11 +0900

    add swagger

--- sub_task/urls.py
+++ sub_task/urls.py
@@ -4,5 +4,5 @@ from sub_task.views import SubTaskListAPIMixins, SubTaskDetailAPIMixins
 
 urlpatterns = [
     path('sub-task/list', SubTaskListAPIMixins.as_view()),
-    path('sub-task/detail/<uuid:pk>', SubTaskDetailAPIMixins.as_view()),
+    path('sub-task/detail/<int:pk>', SubTaskDetailAPIMixins.as_view()),
 ]
