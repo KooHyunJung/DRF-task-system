54325e783d15203bbd3943a3196e221db0a563d4
KooHyunJung <ghj3160@gmail.com>
2023-09-03 17:08:43 +0900

    add make base views

--- sub_task/views.py
+++ sub_task/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import mixins, generics
 
-# Create your views here.
+from sub_task.models import SudTask
+from sub_task.serializers import SudTaskSerializer
+
+
+
+class TaskListAPIMixins(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView
+):
+    queryset = SudTask.objects.all()
+    serializer_class = SudTaskSerializer
+    lookup_field = 'pk'
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class TaskDetailAPIMixins(
+    generics.GenericAPIView,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin
+):
+    queryset = SudTask.objects.all()
+    serializer_class = SudTaskSerializer
+    lookup_field = 'pk'
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
